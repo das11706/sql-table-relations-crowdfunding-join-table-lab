85ab870b8271293dcd505d508a3bac2adf68de89
Daniel Suarez <suarez.daniel101@gmail.com>
2021-08-13 16:20:46 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
   "SELECT title, SUM(amount)
   FROM projects
   INNER JOIN pledges
-  WHERE projects.id = pledges.project_id
+  ON projects.id = pledges.project_id
   GROUP BY title"
 end
 
@@ -18,7 +18,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT name, age, SUM(amount)
   FROM users
   INNER JOIN pledges
-  WHERE users.id = pledges.user_id
+  ON users.id = pledges.user_id
   GROUP BY name"
 end
 
@@ -26,7 +26,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT title, SUM(amount) - funding_goal
   FROM projects
   INNER JOIN pledges
-  WHERE projects.id = pledges.project_id
+  ON projects.id = pledges.project_id
   GROUP BY title
   HAVING SUM(amount) >= funding_goal"
 end
@@ -35,15 +35,23 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT name, SUM(amount)
   FROM users
   INNER JOIN pledges
-  WHERE users.id = pledges.user_id
+  ON users.id = pledges.user_id
   GROUP BY name
   ORDER BY SUM(amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT category, amount
+  FROM projects
+  INNER JOIN pledges
+  ON projects.id = pledges.project_id
+  WHERE category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "Write your SQL query Here"
+  "SELECT category, SUM(amount)
+  FROM projects
+  INNER JOIN pledges
+  ON projects.id = pledges.project_id
+  WHERE category = 'books'"
 end
